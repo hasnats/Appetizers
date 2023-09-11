5b193d89279a4e79f9bac6142a6e54f03c17ca20
Hasna T S <hasnats789@gmail.com>
2023-09-11 17:41:29 +0530

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Hasna T S on 11/09/23.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
